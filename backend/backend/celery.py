c10a50daf0035b078a96751ebcf9b8635d4dee8a
santa4246 <santa4246@naver.com>
2022-02-05 23:39:32 +0900

    celery..

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -9,6 +9,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-# @app.task(bind=True)
-# def debug_task(self):
-#     print(f'Request: {self.request!r}')
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
